c5f65853ab3a02eba61458381a796e07975dbf50
Alex Simisi Nta <alexsms.devops@gmail.com>
2023-01-10 02:09:36 +0100

    import module second subnet in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+## VPC Module
 module "vpc_1_subnet_public" {
   source               = "./modules/vpc_1_subnet_public"
   cidr_block           = var.vpc_cidr
@@ -5,6 +6,7 @@ module "vpc_1_subnet_public" {
   public_subnet_1_name = var.public_subnet_1_name
 }
 
+## EC2 Nginx Module
 module "ec2_nginx" {
   source                = "./modules/ec2_nginx"
   ec2_instance_tag_name = var.ec2_tag_name
@@ -13,8 +15,17 @@ module "ec2_nginx" {
   ec2_subnet_id         = module.vpc_1_subnet_public.subnet_id
 }
 
+## Private subnet Module
 module "private_subnet" {
   source = "./modules/private_subnet"
+  vpc-cidr = var.vpc_cidr
+  vpc-id = module.vpc_1_subnet_public.vpc_id
+  vpc-nat_gateway-id = module.vpc_1_subnet_public.nat_gateway_id
+}
+
+## Second public subnet
+module "second_public_subnet" {
+  source = "./modules/second_subnet_public"
   vpc-cidr = var.vpc_cidr
   vpc-id = module.vpc_1_subnet_public.vpc_id
   vpc-nat_gateway-id = module.vpc_1_subnet_public.nat_gateway_id
